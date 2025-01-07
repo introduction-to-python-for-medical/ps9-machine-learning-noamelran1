d16085aa18371d22904d471595df27571b12d2ac
noamelran1 <thenoamelran@gmail.com>
2025-01-07 21:39:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import pandas as pd
 
 parkinsons_df = pd.read_csv('parkinsons.csv')
-parkinsons_df= parkinsons_df.dropna ()
+parkinsons_df= parkinsons_df.dropna()
 parkinsons_df.head()
-
-input_features = ['PPE', 'DFA']  
-output_feature = 'status' 
-X = parkinsons_df[input_features]
-y = parkinsons_df[output_feature]
+selected_features = ['PPE', 'DFA']
+target= ['status']
+x=parkinsons_df[selected_features]
+y=parkinsons_df[target]
 
 from sklearn.preprocessing import MinMaxScaler
-
 scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+x_scaled = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
-X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 from sklearn.svm import SVC
-
-svc = SVC()
-svc.fit(X_train, y_train)
+svc = SVC ()
+svc.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
-
-y_pred = svc.predict(X_val)
-accuracy = accuracy_score(y_val, y_pred)
-
+y_pred = svc.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
+
+
 import joblib
 
-joblib.dump(SVC, 'svc_model.joblib')
+joblib.dump(svc, 'svc_model.joblib')
