7a15fbef35be3e871f71edeb358ffc40bb1ece54
noamelran1 <thenoamelran@gmail.com>
2025-01-07 21:33:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-%load_ext autoreload
-%autoreload 2
-
-# Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-
-  import pandas as pd
+import pandas as pd
 
 parkinsons_df = pd.read_csv('parkinsons.csv')
+parkinsons_df= parkinsons_df.dropna ()
 parkinsons_df.head()
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-input_features = ['DFA', 'PPE']  
+input_features = ['PPE', 'DFA']  
 output_feature = 'status' 
-
-
 X = parkinsons_df[input_features]
 y = parkinsons_df[output_feature]
 
@@ -23,12 +13,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
-X_scaled
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_val, y_train, y_val = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-
 from sklearn.svm import SVC
 
 svc = SVC()
